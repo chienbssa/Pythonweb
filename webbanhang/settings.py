49e4d7397a91ff68946ae23104bd88f2f74eb3aa
chienbssa <chienbssa@gmail.com>
2026-05-04 15:55:45 +0700

    fix database

--- webbanhang/settings.py
+++ webbanhang/settings.py
@@ -90,9 +90,8 @@ WSGI_APPLICATION = 'webbanhang.wsgi.application'
 # }
 
 DATABASES = {
-'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600
+ 'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
     )
 }
 
